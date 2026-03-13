8d623bbb5d1644fbc2a0cbe9dc44e0c4347c0c92
Marco Piro <marco.piro.1@studenti.unipd.it>
2026-03-13 17:20:41 +0100

    aggiunto serialize tipo

--- src/app/shared/models/result-ai-assistant.model.ts
+++ src/app/shared/models/result-ai-assistant.model.ts
@@ -1,33 +1,30 @@
 import { Result } from './result.model';
 
-
 export interface ResultAiAssistant extends Result {
-  title: string;
-  content: string;
-  imagePath: string;
-  tone: Tone;
-  style: Style;
-  data: Date;
-  prompt: string;
-  evaluation: number;
-
+	title: string;
+	content: string;
+	imagePath: string;
+	tone: Tone;
+	style: Style;
+	data: Date;
+	prompt: string;
+	evaluation: number;
 }
 
-
 enum Style {
-  Concise = 'concise',
-  Descriptive = 'descriptive',
-  Technical = 'technical',
-  Narrative = 'narrative',
-  Creative = 'creative',
-  Academic = 'academic'
+	Concise = 'concise',
+	Descriptive = 'descriptive',
+	Technical = 'technical',
+	Narrative = 'narrative',
+	Creative = 'creative',
+	Academic = 'academic'
 }
 
 enum Tone {
-  Formal = 'formal',
-  Informal = 'informal',
-  Friendly = 'friendly',
-  Professional = 'professional',
-  Persuasive = 'persuasive',
-  Neutral = 'neutral'
+	Formal = 'formal',
+	Informal = 'informal',
+	Friendly = 'friendly',
+	Professional = 'professional',
+	Persuasive = 'persuasive',
+	Neutral = 'neutral'
 }
