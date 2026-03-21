67d0aa3a40240ed4913f4dbd1f25480d15dfb67b
Marco Piro <marco.piro.1@studenti.unipd.it>
2026-03-21 17:04:24 +0100

    tolto +Nuovo su menutendina in estrattore

--- src/app/components/menutendina/menutendina.ts
+++ src/app/components/menutendina/menutendina.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SelectModule } from 'primeng/select';
 import { IftaLabelModule } from 'primeng/iftalabel';
@@ -7,7 +8,7 @@ import { Button } from '../button/button';
 
 @Component({
   selector: 'menutendina',
-  imports: [FormsModule, SelectModule, IftaLabelModule, ButtonModule, Button],
+  imports: [CommonModule, FormsModule, SelectModule, IftaLabelModule, ButtonModule, Button],
   templateUrl: './menutendina.html',
   styleUrl: './menutendina.css',
 })
@@ -17,6 +18,7 @@ export class Menutendina {
   @Input() selected: any;
   @Input() placeholder: string = '';
   @Input() addButtonLabel: string = 'Nuovo';
+  @Input() showAddNew: boolean = true;
   @Input() disabled: boolean = false;   /** Rende la select parametrica per usarla in più pagine */
   @Output() selectedChange = new EventEmitter<any>();
   @Output() addNew = new EventEmitter<void>();
