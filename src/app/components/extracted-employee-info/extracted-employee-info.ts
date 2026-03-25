c7e50c5339b8887159d1136bda1303bfc0d70506
Linor <lindorlinor@gmail.com>
2026-03-25 18:46:31 +0100

    prima bozza del componente dove si visualizza il destinatario estratto

---
+++ src/app/components/extracted-employee-info/extracted-employee-info.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bb-extracted-employee-info',
+  imports: [],
+  templateUrl: './extracted-employee-info.html',
+  styleUrl: './extracted-employee-info.css',
+})
+export class ExtractedEmployeeInfo {
+
+}
