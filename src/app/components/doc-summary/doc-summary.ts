a98c0a7dba21abf0b8c8d20b3ead323962e24837
Linor <lindorlinor@gmail.com>
2026-03-26 14:28:47 +0100

    creata la componente di preview pdf

--- src/app/components/doc-summary/doc-summary.ts
+++ src/app/components/doc-summary/doc-summary.ts
@@ -4,9 +4,10 @@ import { Button } from '../button/button';
 import { StatusPill } from '../status-pill/status-pill';
 import { ResultSplit } from '../../shared/models/result-split.model';
 import { PageRangeInput } from '../page-range-input/page-range-input';
+import { PdfPreview } from '../pdf-preview/pdf-preview';
 @Component({
   selector: 'bb-doc-summary',
-  imports: [InputComponent, Button, StatusPill, PageRangeInput],
+  imports: [InputComponent, Button, StatusPill, PageRangeInput, PdfPreview],
   templateUrl: './doc-summary.html',
   styleUrl: './doc-summary.css',
 })
@@ -15,5 +16,6 @@ export class DocSummary {
   @Output() openOriginalPdf = new EventEmitter<void>();
   @Output() openSplitPdf = new EventEmitter<void>();
 
+  pdfUrl: string = 'prova.pdf';
 
 }
