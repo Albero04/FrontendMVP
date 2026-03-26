fb5502e10a2a5911476d80f235cc5fe467cee234
Linor <lindorlinor@gmail.com>
2026-03-26 10:43:25 +0100

    migliorato logica app-input

--- src/app/components/input/input.ts
+++ src/app/components/input/input.ts
@@ -10,15 +10,17 @@ import { InputTextModule } from "primeng/inputtext";
 })
 export class InputComponent {
   @Input() label: string = "";
-  @Input() value: string = "";
+  @Input() value: string | number | undefined= "";
   @Input() placeholder: string = "";
   @Input() type: string = "text";
+  @Input() min: string | number | null = null;
+  @Input() max: string | number | null = null;
   @Input() id: string = "inputField";
   @Input() disabled: boolean = false;
   @Input() class: string = "";
-  @Output() valueChange = new EventEmitter<string>();
+  @Output() valueChange = new EventEmitter<string | number |undefined>();
 
-  onValueChange(value: string): void {
+  onValueChange(value: string | number | undefined): void {
     this.valueChange.emit(value);
   }
 }
